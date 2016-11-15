80c0f828ce3c31a790cd3232d4b1758de8fdfa48
Kevin Merckx <kevin@interfacewerk.de>
2016-11-15 15:48:00 +0100

    fix test

--- src/stateful-button/stateful-button.directive.spec.ts
+++ src/stateful-button/stateful-button.directive.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { StatefulButtonDirective } from './stateful-button.component';
+import { StatefulButtonDirective } from './stateful-button.directive';
 
 describe('StatefulButtonComponent', () => {
   let component: StatefulButtonDirective;
